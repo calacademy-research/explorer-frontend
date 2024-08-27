848610a6902ce07e073f83d405d0b8ce05236fe8
arthur <arthurjdam@gmail.com>
2024-08-27 15:37:36 -0700

    chore: lint

--- src/components/CommandMenu/CommandMenu.tsx
+++ src/components/CommandMenu/CommandMenu.tsx
@@ -1,44 +1,48 @@
-"use client";
+'use client';
 
-import { Command } from "cmdk";
-import { useMemo, useState } from "react";
+import { Command } from 'cmdk';
+import { useMemo, useState } from 'react';
 
 interface MenuOption {
-  id: string;
-  label: string;
+    id: string;
+    label: string;
 }
 
 interface CommandMenuProps {
-  options: Array<MenuOption>;
+    options: Array<MenuOption>;
 }
 
 const CommandMenu: React.FC<CommandMenuProps> = ({ options }) => {
-  const [query, setQuery] = useState("");
-  const filteredResults = useMemo(
-    () =>
-      query.length === 0
-        ? options
-        : options.filter((option) => {
-            return option.label.toLowerCase().includes(query.toLowerCase());
-          }),
-    [query, options],
-  );
+    const [query, setQuery] = useState('');
+    const filteredResults = useMemo(
+        () =>
+            query.length === 0
+                ? options
+                : options.filter((option) =>
+                      option.label.toLowerCase().includes(query.toLowerCase())
+                  ),
+        [query, options]
+    );
 
-  return (
-    <Command label="Command Menu" shouldFilter={false}>
-      <Command.Input value={query} onValueChange={setQuery} />
-      <Command.List>
-        <Command.Empty>No results found.</Command.Empty>
-        <Command.Group heading="Letters">
-          {filteredResults.map((result) => (
-            <Command.Item key={result.id}>{result.label}</Command.Item>
-          ))}
-          <Command.Separator alwaysRender={filteredResults.length > 0} />
-          {/* <Command.Item value={query}>Add “{query}”</Command.Item> */}
-        </Command.Group>
-      </Command.List>
-    </Command>
-  );
+    return (
+        <Command label="Command Menu" shouldFilter={false}>
+            <Command.Input value={query} onValueChange={setQuery} />
+            <Command.List>
+                <Command.Empty>No results found.</Command.Empty>
+                <Command.Group heading="Letters">
+                    {filteredResults.map((result) => (
+                        <Command.Item key={result.id}>
+                            {result.label}
+                        </Command.Item>
+                    ))}
+                    <Command.Separator
+                        alwaysRender={filteredResults.length > 0}
+                    />
+                    {/* <Command.Item value={query}>Add “{query}”</Command.Item> */}
+                </Command.Group>
+            </Command.List>
+        </Command>
+    );
 };
 
 export default CommandMenu;
