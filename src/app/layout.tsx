a114605eb1dd91af6e54f46491044687db4ec71e
arthur <arthurjdam@gmail.com>
2025-01-14 15:09:18 -0800

    feat: add model viewer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import MainSelector from '@/components/MainSelector/MainSelector';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+           <Script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/4.0.0/model-viewer.min.js" />
             <body className={inter.className}>
                 <MainSelector />
                 {children}
